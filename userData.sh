b377a71f138c7c3198279df8dbe1f7f6ea8a61a1
Ajay Tekam <33233365+Ajaytekam@users.noreply.github.com>
2022-12-08 14:07:12 +0530

    Update userData.sh
    script updated

--- userData.sh
+++ userData.sh
@@ -12,3 +12,5 @@ sudo yum-config-manager \
 sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin   
 curl -L https://github.com/docker/compose/releases/download/$VERSION/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo systemctl start docker
+sudo systemctl enable docker
